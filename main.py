f7010a1df396ac11fb6068fc1c536847d4c1ce0d
emkr-13 <krisna.rata13@gmail.com>
2024-09-04 10:16:55 +0700

    update aja

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from twscrape.logger import set_log_level
 async def main():
     api = API()
     # ADD ACCOUNTS (for CLI usage see BELOW)
-    await api.pool.add_account("WahyudiMil23368", "icaimutt", "cevogemozon@idemainc.com", "icaimutt")
+    # await api.pool.add_account("WahyudiMil23368", "icaimutt", "cevogemozon@idemainc.com", "icaimutt")
     await api.pool.login_all()
 
 if __name__ == "__main__":
